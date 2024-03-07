884051bd650ed5322538957e79b6b73d9ae3751b
Lilly Hewitt <70710764+lillyhewitt@users.noreply.github.com>
2024-03-07 18:33:29 -0500

    Create Temperature.py

---
+++ Messy_Chocolate/Temperature.py
@@ -0,0 +1,6 @@
+def main():
+print(" ")
+print("Welccome to the Celsius to Fahrenheit converter!")
+tempC = float(input("Please enter a temperature in Celsius: "))
+tempF = (tempC * (9/5) + 32)
+print(tempF , " degrees in Fahrenheit")

b2167cbf2561d3831beac2bbc790855fb9817ad9
Lilly Hewitt <70710764+lillyhewitt@users.noreply.github.com>
2024-03-07 19:07:53 -0500

    Create hello.py

---
+++ HelloAll/hello.py
@@ -0,0 +1,24 @@
+#Lilly Hewitt
+print("James Gosling is known for founding Java.\nHe is known as Dr.Java for his
+creation.\nHe released this programming language in 1996.")
+q = input("Have you heard of thid person? ")
+print(q)
+print("They are pretty cool, aren't they.")
+
+#part 1.1 example code
+#text = input("Tell me a secret: ")
+#print("Your secret was: ")
+#print(text)
+#print("Whoops. I'm not good at keeping secrets...")
+
+#part 2
+print(" _______________")
+print("| |")
+print("| _ _ _ |")
+print("| | | | | | | |")
+print("| - - - |")
+print("| |")
+print("| _ ___ _ |")
+print("| | | | | | | |")
+print("| - | | - |")
+print(" _______________")
